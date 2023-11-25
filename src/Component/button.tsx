faae591799b67b28cc175d86740bd469242b33d4
mahsa <mahsaadlooo@gmail.com>
2023-11-25 13:34:31 -0800

    register form

---
+++ src/Component/button.tsx
@@ -0,0 +1,18 @@
+import React, {type FC, type ReactNode} from 'react'
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    varient?: 'contained' | 'outline' | 'text';
+    children: ReactNode;
+}
+
+const Button: FC<ButtonProps> = ({children, varient, ...restProps}) => {
+    const buttonStyle = {
+        contained: 'border-none bg-rose-600 text-slate-50 hover:bg-rose-700 active:bg-rose-900',
+        outline: 'border border-rose-600 text-rose-600 hover:bg-rose-600 hover:text-slate-50 active:bg-rose-900 active:text-slate-50',
+        text: 'border-none bg-rose-300 text-slate-800 hover:bg-rose-400 active:bg-rose-500'
+    };
+    return (
+        <button {...restProps} className= {`${buttonStyle[varient ?? "contained"]} !${restProps.className} py-3 px-5 rounded-md`}> {children} </button>
+    )
+}
+export default Button;
--- src/Pages/register.tsx
+++ src/Pages/register.tsx
@@ -1,10 +1,20 @@
-import {type FC} from 'react'
+import React, {type FC} from 'react'
+import Textfield from '../Component/textfield';
+import Button from '../Component/button';
 
 const RegisterPage: FC = () => {
     return <div className='h-screen flex'>
-        <img src="/register.jpg" alt="" className='flex-1 object-cover'/>
+        <img className='flex-1 object-cover' src="/register.jpg" alt="" />
         <form className='flex-1' action="">
-
+            <h3> Sign Up Form!</h3>
+            <Textfield type="text" placeholder='Johnsen James' label="Enter your Name" />
+            <Textfield type="email" placeholder='JohnsenJames@text.com' label="Enter your Email" />
+            <Textfield type='radio' label="Enter your Gender" />
+            <Textfield type="text" placeholder='Message' label="Enter your Address" />
+            <Textfield type="tel" placeholder='09*********' label="Enter your Phone Number" />
+            <Textfield type="password" placeholder='At least 5 characters' label="Enter your Password" />
+            <Textfield type="password" placeholder='Johnsen' label="Repeat Password" />
+            <Button> Sign Up </Button>
         </form>
     </div>
 }
